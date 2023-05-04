7fde5f34ef4fe185bff11b34b0913b3bc4354e24
TomaszOlek <tomasz.olek.dev@gmail.com>
2023-05-04 19:56:35 +0200

    Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+  let lowPoint = 0;
+  let highPoint = haystack.length;
+
+  do {
+    const midPoint = Math.floor(lowPoint + (highPoint - lowPoint) / 2)
+    const midValue = haystack[midPoint]
+
+    if (midValue === needle) {
+      return true
+    } else if (midValue > needle) {
+      highPoint = midPoint
+    } else {
+      lowPoint = midPoint + 1
+    }
+  } while (lowPoint < highPoint)
+
+  return false
 }
---
+++ src/day1/Search.md
@@ -0,0 +1,9 @@
+## Linear Serch O(N) //LinearSearchList
+
+Start from 0 go throu all the arrey ex.Indexof
+
+## Binary Search O(log N) //
+
+# Has to be ordered //BinarySearchList
+
+With every iteration, we check if the thing we are looking for is in the first half or in the second, and we repeat this until we find the item or there is no items left
